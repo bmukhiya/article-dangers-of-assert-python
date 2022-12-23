b16802fb034570f6760dd51cff963cad55cff4ae
bmukhiya <96289678+bmukhiya@users.noreply.github.com>
2022-12-23 13:04:02 +0530

    Update safe_assert_example.py

--- safe_assert_example.py
+++ safe_assert_example.py
@@ -6,7 +6,7 @@ def simpleInterest(p, t, r):
 	print('The rate of interest is', r)
 	assert isinstance(r, int) and r > 0, "Rate of interest must be a positive integer"
 	simpleInterest = (p * t * r) / 100
-	print('The Simple Interest is', simpleInterest)
+	print('The Simple Interest', simpleInterest)
 	return simpleInterest
 
 simpleInterest(3, 5, 8)
